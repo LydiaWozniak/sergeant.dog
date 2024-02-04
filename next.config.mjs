d9a204352af1426b87b21b1e81787a29e327f4fd
Braden Marshall <braden1996@hotmail.co.uk>
2024-02-04 14:56:16 +0000

    Switch React to Preact (this may not work)

---
+++ next.config.mjs
@@ -0,0 +1,25 @@
+import bundleAnalyzer from "@next/bundle-analyzer";
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+
+  webpack: (config, { dev, isServer }) => {
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        "react/jsx-runtime.js": "preact/compat/jsx-runtime",
+        react: "preact/compat",
+        "react-dom/test-utils": "preact/test-utils",
+        "react-dom": "preact/compat",
+      });
+    }
+    return config;
+  },
+};
+
+const withBundleAnalyzer = bundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
+});
+
+export default withBundleAnalyzer(nextConfig);
