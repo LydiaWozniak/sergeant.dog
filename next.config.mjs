2920b22516737eb8781c9bd8af1425995272575c
Braden Marshall <braden1996@hotmail.co.uk>
2024-02-04 15:04:28 +0000

    Ditch Preact and use Million for no reason tbh

--- next.config.mjs
+++ next.config.mjs
@@ -1,25 +1,18 @@
+import million from "million/compiler";
 import bundleAnalyzer from "@next/bundle-analyzer";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+};
 
-  webpack: (config, { dev, isServer }) => {
-    if (!dev && !isServer) {
-      Object.assign(config.resolve.alias, {
-        "react/jsx-runtime.js": "preact/compat/jsx-runtime",
-        react: "preact/compat",
-        "react-dom/test-utils": "preact/test-utils",
-        "react-dom": "preact/compat",
-      });
-    }
-    return config;
-  },
+const millionConfig = {
+  auto: { rsc: true },
 };
 
 const withBundleAnalyzer = bundleAnalyzer({
   enabled: process.env.ANALYZE === "true",
 });
 
-export default withBundleAnalyzer(nextConfig);
+export default withBundleAnalyzer(million.next(nextConfig, millionConfig));
